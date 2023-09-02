a9ca565df15a008fa0fe59d1b85e68db92b3c79c
DataStas <sizovstanislav10@gmail.com>
2023-09-02 08:15:07 +0000

    =

--- Leetcode/Yandex_tasks/test.py
+++
@@ -1,9 +0,0 @@
-import random
-
-a = [random.randint(1, 100) for _ in range(50)]
-tmp = a[0]
-for ind in range(1, len(a)):
-    if a[ind] > tmp:
-        tmp1 = tmp
-        tmp = a[ind]
-print(tmp, tmp1)
